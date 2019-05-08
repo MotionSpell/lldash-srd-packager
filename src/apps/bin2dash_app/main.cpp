f170bc819fb53c8a01000b3202e5ecb75340b936
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-05-08 20:35:02 +0200

    make bin2dash_app independent from Signals

--- src/apps/bin2dash_app/main.cpp
+++ src/apps/bin2dash_app/main.cpp
@@ -1,14 +1,43 @@
-#include "file_reader.hpp"
 #include "../bin2dash/bin2dash.hpp"
-#include "lib_appcommon/options.hpp"
-#include "lib_utils/format.hpp"
-#include "lib_utils/tools.hpp"
-#include "lib_utils/system_clock.hpp"
+#include <chrono>
+#include <cstdio>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
+#include <experimental/filesystem>
+
+using namespace std::experimental::filesystem::v1;
 
 const char *g_appName = "bin2dash_app";
+
+namespace {
+std::vector<std::string> resolvePaths(std::string path) {
+	std::vector<std::string> res;
+	if (!path.empty()) {
+		for (const auto & entry : directory_iterator(path)) {
+			res.push_back(entry.path().string());
+		}
+	}
+	return res;
+}
+
+void fillVector(std::string fn, std::vector<uint8_t> &buf) {
+	auto file = fopen(fn.c_str(), "rb");
+	if (!file)
+		throw std::runtime_error(std::string("Can't open file for reading: \"") + fn + "\"");
+	fseek(file, 0, SEEK_END);
+	auto const size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	buf.resize(size);
+	int read = (int)fread(buf.data(), 1, size, file);
+	if (read != size)
+		throw std::runtime_error(std::string("Can't read enoug data for file \"") + fn + "\" (read=" + +"bytes)");
+	fclose(file);
+}
+}
 
 struct Config {
 	std::string inputPath;
@@ -17,27 +46,45 @@ struct Config {
 	std::string publish_url;
 };
 
+static void usage() {
+	fprintf(stderr, "Usage: %s [options, see below] [file_pattern]\n", g_appName);
+	Config cfg;
+	fprintf(stderr, "\t-d\tdurationInMs: 0: segmentTimeline, otherwise SegmentNumber[default=%d]\n", cfg.segDurInMs);
+	fprintf(stderr, "\t-s\tsleepAfterFrameInMs: sleep time in ms after each frame, used for regulation[default=%d]\n", cfg.sleepAfterFrameInMs);
+	fprintf(stderr, "\t-u\tpublishURL: if empty files are written and the node-gpac-http server should be used, otherwise use the Evanescent SFU.[default=\"%s\"]\n", cfg.publish_url.c_str());
+}
+
 std::unique_ptr<const Config> args(int argc, char const* argv[]) {
-	CmdLineOptions opt;
-	auto opts = uptr(new Config);
+	auto opts = std::make_unique<Config>();
 
-	opt.add("d", "durationInMs"       , &opts->segDurInMs         , format("0: segmentTimeline, otherwise SegmentNumber [default=%s]", opts->segDurInMs));
-	opt.add("s", "sleepAfterFrameInMs", &opts->sleepAfterFrameInMs, format("Sleep time in ms after each frame, used for regulation [default=%s]", opts->sleepAfterFrameInMs));
-	opt.add("u", "publishURL"         , &opts->publish_url, format("Publish URL. If empty files are written and the node-gpac-http server should be used, otherwise use the Evanescent SFU. [default=\"%s\"]", opts->publish_url));
-
-	std::vector<std::string> files;
-	try {
-		files = opt.parse(argc, argv);
-	} catch (std::exception &e) {
-		opt.printHelp(std::cerr);
-		throw e;
+	int argIdx = 0;
+	while (++argIdx < argc) {
+		if (!strcmp("-d", argv[argIdx])) {
+			if (++argIdx >= argc) {
+				usage();
+				throw std::runtime_error("Missing argument after \"-d\". Aborting.");
+			}
+			opts->segDurInMs = atoi(argv[argIdx]);
+			fprintf(stderr, "Detected segment duration of %dms\n", opts->segDurInMs);
+		} else if (!strcmp("-s", argv[argIdx])) {
+			if (++argIdx >= argc) {
+				usage();
+				throw std::runtime_error("Missing argument after \"-s\". Aborting.");
+			}
+			opts->sleepAfterFrameInMs = atoi(argv[argIdx]);
+			fprintf(stderr, "Detected sleep after frame of %dms\n", opts->sleepAfterFrameInMs);
+		} else if (!strcmp("-u", argv[argIdx])) {
+			if (++argIdx >= argc) {
+				usage();
+				throw std::runtime_error("Missing argument after \"-u\". Aborting.");
+			}
+			opts->publish_url = argv[argIdx];
+			fprintf(stderr, "Detected publish URL \"%s\"\n", opts->publish_url.c_str());
+		} else {
+			opts->inputPath = argv[argIdx];
+			fprintf(stderr, "Detected input path \"%s\"\n", opts->inputPath.c_str());
+		}
 	}
-	if (files.size() != 1) {
-		fprintf(stderr, "Usage: %s [options, see below] [file_pattern]\n", g_appName);
-		opt.printHelp(std::cerr);
-		throw std::runtime_error("Invalid command line");
-	}
-	opts->inputPath = files[0];
 
 	return std::move(opts);
 }
@@ -59,7 +106,7 @@ int main(int argc, char const* argv[]) {
 			vrt_push_buffer(handle, buf.data(), buf.size());
 
 			if (config->sleepAfterFrameInMs)
-				g_SystemClock->sleep(Fraction(config->sleepAfterFrameInMs, 1000));
+				std::this_thread::sleep_for(std::chrono::milliseconds(config->sleepAfterFrameInMs));
 
 			i++;
 		}
